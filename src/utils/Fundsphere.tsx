605d0e07112f70ad1626d4d030962984f33b8bed
Imanuel Revo Admojo <imanuelrevoadmojo@gmail.com>
2025-02-08 19:09:07 +0700

    Update Fundsphere.tsx

--- src/utils/Fundsphere.tsx
+++ src/utils/Fundsphere.tsx
@@ -1,26 +1,22 @@
 import { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import '../index.css'
-import '../global.css'
+import '../index.css';
 import Amazon from '../assets/amazon.svg';
 import Ebay from '../assets/ebay.svg';
 import Uber from '../assets/uber.svg';
 import Walmart from '../assets/walmart.svg';
 import Logo from '../utils/Logo.svg';
 import MoneyCharger from '../assets/moneycharger.svg';
-import ConstRedution from '../assets/constredution.svg';
-import MaskGroup from '../assets/Mask group.svg';
-import VectorLeft from '../assets/vectorleft.svg';
-import VectorRight from '../assets/vectorright.svg';
-import HeroGradient from '../assets/herogradient.svg';
+import Costreduction from '../assets/constredution.svg';
+import MaskGrup from '../assets/Mask group.svg';
 
 
 gsap.registerPlugin(ScrollTrigger);
 
 function Fundsphere() {
 
-    
+
     useEffect(() => {
         const sections = document.querySelectorAll(".slide-in");
 
@@ -60,9 +56,9 @@ function Fundsphere() {
                                                 Features ▾
                                             </a>
                                             <ul className="dropdown-menu">
-                                                <li><a href="#">Feature 1</a></li>
-                                                <li><a href="#">Feature 2</a></li>
-                                                <li><a href="#">Feature 3</a></li>
+                                                <li><a href="#">Landing Page V1</a></li>
+                                                <li><a href="#">Landing Page V2</a></li>
+                                                <li><a href="#">Landing Page V3</a></li>
                                             </ul>
                                         </li>
                                         <li><a className="nav-link" href="#">Pricing</a></li>
@@ -86,25 +82,24 @@ function Fundsphere() {
                 <div className="container">
                     <div className="hero-home-container">
                         <div className="hero-home">
-                            <img src={HeroGradient} alt="hero-gradient" />
                             <img src={MoneyCharger} alt="money-charger" />
-                            <div className="image-wrapper-v2">
-                                <img src={ConstRedution} alt="const-redution" />
-                                <img src={MaskGroup} alt="mask-group" />
+                            <div className="griden-comp">
+                                <img src={Costreduction} alt="cost-reduct" />
+                                <img src={MaskGrup} alt="mask-grup" />
                             </div>
                         </div>
                         <div className="hero-home-content">
                             <div className="hero-heading">
-                                <div className="label-badge">
+                                <div className="label-badge-head">
                                     <text>BEST CHOICE</text>
                                 </div>
-                                <h1>Future strategic finance for <text>Enterprenurs</text></h1>
+                                <h1>Future strategic finance for <br /><text>Enterprenurs</text></h1>
                                 <p>Scale with checking and savings accounts, custom tools, and access to our investor network.</p>
                             </div>
                             <form action="input">
                                 <div className="cta-btn-wrapper">
                                     <button type="submit">Get Started</button>
-                                    <input type="Your work email" />
+                                    <input type="text" placeholder="Your work email" />
                                 </div>
                             </form>
                             <div className="overview-content">
@@ -121,8 +116,6 @@ function Fundsphere() {
                                     <p>Country Available</p>
                                 </div>
                             </div>
-                            <img src={VectorLeft} alt="vector-left" />
-                            <img src={VectorRight} alt="vector-right" />
                         </div>
                     </div>
                 </div>
@@ -130,8 +123,8 @@ function Fundsphere() {
 
 
             {/* section overview */}
-            <section>
-                <div className="container">
+            <section className="section-overview">
+                <div className="container-overview">
                     <div className="overview-content">
                         <div className="company-logo">
                             <img src={Uber} alt="uber-logo" />
@@ -145,14 +138,14 @@ function Fundsphere() {
 
             {/* section features */}
 
-            <section>
-                <div className="container">
-                    <div className="subsecsection-heading">
-                        <div className="label-badge">
+            <section className="sec-features">
+                <div className="container-features">
+                    <div className="subsection-heading">
+                        <div className="label-badge-feature">
                             <text>FEATURES</text>
                         </div>
-                        <h2>Features designed for you</h2>
-                        <p>Growth-accelerating products for startups, ecommerce stores, angel investors, & more.</p>
+                        <h2>Features <br />designed for you</h2>
+                        <p>Growth-accelerating products for startups,<br /> ecommerce stores, angel investors, & more.</p>
                     </div>
                     <div className="features-content">
                         <div className="features-wrapper-up">
@@ -173,9 +166,60 @@ function Fundsphere() {
                                 </div>
                             </div>
                         </div>
-                        <div className="features-wrapper"></div>
+                        <div className="features-wrapper-low">
+                            <div className="features-card-wrapper-3">
+                                <div className="features-card-heading">
+                                    <h3>Integrates with best of breed solution</h3>
+                                    <p>Integrate with best of payment processors and accounting software.</p>
+                                </div>
+                            </div>
+                            <div className="features-card-wrapper-4">
+                                <div className="features-card-heading">
+                                    <h3>Cost Reduction</h3>
+                                    <p>Fundsphere Reduced Payments maintenance and processing fees. No hidden fees.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <button className="btn-primary">See More Features</button>
+                </div>
+            </section>
+
+            {/* section pricing plan */}
+
+            <section className="sec-pricing">
+                <div className="container-pricing">
+                    <div className="sub-head-pricing">
+                        <div className="label-badge-pricing">
+                            <text>PRICING PLAN</text>
+                        </div>
+                        <h2>Choose Package</h2>
+                        <p>Join our pro features for unlimited<br /> without any contract and cancel anytime.</p>
+                    </div>
+                    <form action="form-block">
+                        <div className="form-switcher">
+                            <span>Monthly</span>
+                            <label className="toggle-switch">
+                                <input type="checkbox" />
+                                <span className="slider"></span>
+                            </label>
+                            <span>Yearly</span>
+                        </div>
+                    </form>
+                    <div className="pricing-content">
+                        <div className="package-card">
+                            <div className="package-gap-2">
+                                <div className="package-heading">
+                                    <h3 className="sub-head">Basic</h3>
+                                    <p className="sub-desc">Kickstart product research <br/>in your business at no cost.</p>
+                                </div>
+                                <ul>
+                                    <li><text>Unlimited workspace</text></li>
+                                    <li><text>Access to RestAPI</text></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
